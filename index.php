840aae300c322c1820db0e99faf056f0b4fddeee
dbcrea <djelloul.be@gmail.com>
2021-07-21 16:05:58 +0200

    bootstrap setting and exercice 4

--- index.php
+++ index.php
@@ -5,10 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer></script>
+
+
 </head>
 <body>
-<h1>Hello world !</h1>
-<p>
+<div class="container">
+<h1 class="text-center">Bonnets Store</h1>
+<p class="text-center">
  <?php echo date('d-m-d H:i:s');?>
  </p> 
  <?php
@@ -44,18 +49,29 @@ $bonnets = [
 
 ?>
 <br>
-<table>
+<table class="table">
     <thead>
-        <tr>
-            <th colspan="2">id</th>
-            <th colspan="2">Désignation</th>
-            <th colspan="2">Prix</th>
+        <tr class="table-dark">
+            <th  colspan="2">#</th>
+            <th  colspan="2">Désignation</th>
+            <th colspan="2">Prix HT</th>
+            <th colspan="2">Prix TTC</th>
             <th colspan="2">description</th>
         </tr>
         
     </thead>
     <tbody>
         <?php
+        //exercice 4
+        
+        function prixHt( $prixTtc){
+            return $prixTtc /1.2;
+        }
+
+       
+
+
+        //Exercice 3
         foreach($bonnets as $i => $item){
             $color = '';
 
@@ -65,24 +81,32 @@ $bonnets = [
                 $color ='red';
             }
 
-            ?>
+            $prixTtc =  prixHt( $item['prix']);
 
+            ?>
 
            <tr>
                     <td  colspan="2"><?= $i?> </td> 
                     <td colspan="2"><?= $item['designation'] ?> </td>
+                    <td colspan="2"><?= number_format($prixTtc, 2) ?> €</td>
+                    <td style="color: <?php echo $color ?>;" 
+                    colspan="2"><?= $item['prix'] ?> €</td>
 
-                    <td style="color: <?php echo $color ?>;" colspan="2"><?= $item['prix'] ?> </td>
                     <td colspan="2"><?= $item['description'] ?> </td>
             <tr>
 
         <?php
             }
+
+
+
         ?>               
         
 
     </tbody>
 </table>
+
+</div>
 
 </body>
 </html>
